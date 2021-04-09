e8b674c3821cd84804d72f5fbea99441a4f2fad7
sanukin39 <ericsson.goodjob@gmail.com>
2021-04-10 02:57:26 +0900

    support uv

--- Assets/UniPlaneMeshGenerator/Runtime/PlaneMeshGenerator.cs
+++ Assets/UniPlaneMeshGenerator/Runtime/PlaneMeshGenerator.cs
@@ -6,10 +6,17 @@ namespace UPMG
     {
         public static Mesh GenerateHorizontal(int width, int height, float unitLength, bool isHorizontal)
         {
+            var halfWidth = width * unitLength / 2f;
+            var halfHeight = height * unitLength / 2f;
+            var positionDelta =
+                isHorizontal ? new Vector3(-halfWidth, 0, -halfHeight) : new Vector3(-halfWidth, -halfHeight); 
+            width++;
+            height++;
             var direction = isHorizontal ? Vector3.forward : Vector3.up;
             var verticesCount = width * height;
             var triangleCount = (width - 1) * (height - 1) * 2;
             var vertices = new Vector3[verticesCount];
+            var uvs = new Vector2[verticesCount];
             var triangles = new int[triangleCount * 3];
             var trisIndex = 0;
             for (var w = 0; w < width; w++)
@@ -18,8 +25,8 @@ namespace UPMG
                 {
                     var vertIndex = h * width + w;
                     var position = Vector3.right * w * unitLength + direction * h * unitLength;
-                    vertices[vertIndex] = position;
-
+                    vertices[vertIndex] = position + positionDelta;
+                    uvs[vertIndex] = new Vector2(w / (width - 1f), h / (height - 1f));
                     if (w == width - 1 || h == height - 1)
                     {
                         continue;
@@ -34,7 +41,7 @@ namespace UPMG
                 }
             }
 
-            var mesh = new Mesh {vertices = vertices, triangles = triangles};
+            var mesh = new Mesh {vertices = vertices, triangles = triangles, uv = uvs};
             return mesh;
         }
     }
